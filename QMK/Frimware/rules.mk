fd0683ef6529e8607269535ee2d7ab76669e5e97
davidPeryRO <davidperyro@gmail.com>
2026-08-26 00:23:55 +0300

    Delete QMK/Frimware directory

--- QMK/Frimware/rules.mk
+++
@@ -1,6 +0,0 @@
-# This file is intentionally mostly empty.
-# Everything QMK needs (MCU, bootloader, matrix pins, features) is
-# declared in info.json since RP2040 boards use the data-driven build.
-#
-# Add make-time only options here if you need them later, e.g.:
-# LTO_ENABLE = yes
